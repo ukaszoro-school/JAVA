139e57ccb608ef37cf50abe784336cc00e56e5ec
UK-laptop <lukasz.les32@student.pk.edu.pl>
2024-11-28 17:56:56 +0100

    zad 2 fix
    update log:
    - added few lines to main to show the ability to print out record Color values
    - updated the after-running.PNG showing the state of the console after running the program

--- lab_2/src/main/java/Main.java
+++ lab_2/src/main/java/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         Color new_color = new Color(143,255,100,155);
-        System.out.println(new_color.toString());
+        System.out.println(new_color);
 
         Color new_color2 = new Color(200,-5,2951);
-        System.out.println(new_color2.toString());
+        System.out.println(new_color2);
+
+        System.out.println("Color 1: " + new_color.alfa() + ", " + new_color.red() + ", " + new_color.green() + ", " + new_color.blue());
+        System.out.println("Color 2: " + new_color2.alfa() + ", " + new_color2.red() + ", " + new_color2.green() + ", " + new_color2.blue());
     }
 }
