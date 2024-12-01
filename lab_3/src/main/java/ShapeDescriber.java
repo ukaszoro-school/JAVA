ca9de64cf28a372b0117fead82a3516a5cee2c05
UK-laptop <lukasz.les32@student.pk.edu.pl>
2024-12-01 10:08:51 +0100

    Zad 3
    update log:
    - fixed a minor mistake when implementing Logging

--- lab_3/src/main/java/ShapeDescriber.java
+++ lab_3/src/main/java/ShapeDescriber.java
@@ -1,16 +1,15 @@
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ShapeDescriber {
-    private static final Logger logger = Logger.getLogger(ShapeDescriber.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(ShapeDescriber.class.getName());
 
     static void describe(Shape tmp_obj) {
-        logger.setLevel(Level.INFO);
         try {
             System.out.printf("Class is a %s\n  Area: %f \n  Perimeter: %f\n  Color: %s \n\n", tmp_obj, tmp_obj.getArea(), tmp_obj.getPerimeter(), tmp_obj.Shape_color.toString());
         }
         catch (Exception e) {
-            logger.warning("Attempted to describe shape '" + tmp_obj + "' failed with error: " + e.getMessage());
+            logger.warn("Attempted to describe shape '" + tmp_obj + "' failed with error: " + e.getMessage());
         }
         finally {
             logger.info("Class '" + tmp_obj + "' with Area: '" + tmp_obj.getArea() + "' Perimeter: '" + tmp_obj.getPerimeter() + "' and Color: '" + tmp_obj.Shape_color.toString() + "' has been successfully described.");
